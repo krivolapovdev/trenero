cfd59f6b362d273009a8558a97105f17c623df05
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-11-26 20:41:21 +0500

    back: Add Spring Boot Actuator configuration

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.modulith:spring-modulith-starter-core:$springModulithVersion")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openApiVersion")
     implementation("org.postgresql:postgresql")
