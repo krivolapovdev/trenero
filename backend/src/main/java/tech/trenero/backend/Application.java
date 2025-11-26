6b12e0f0b2960496132429471565cd9f7453d094
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-11-26 20:36:16 +0500

    back: Rename BackendApplication to Application for consistency

--- backend/src/main/java/tech/trenero/backend/Application.java
+++ backend/src/main/java/tech/trenero/backend/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Application {
   static void main(String[] args) {
-    SpringApplication.run(BackendApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
