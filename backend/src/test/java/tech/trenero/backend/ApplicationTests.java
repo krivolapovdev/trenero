1f2ba436adc7ec51d887159bdb6c417b0ce13a20
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-11-26 20:38:28 +0500

    back: Add unit test for application module verification

---
+++ backend/src/test/java/tech/trenero/backend/ApplicationTests.java
@@ -0,0 +1,14 @@
+package tech.trenero.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+
+@SpringBootTest
+class ApplicationTests {
+  @Test
+  void shouldPassModuleVerification() {
+    ApplicationModules modules = ApplicationModules.of(Application.class);
+    modules.verify();
+  }
+}
