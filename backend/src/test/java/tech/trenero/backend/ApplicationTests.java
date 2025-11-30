f3cdf4477a0f8d59da719b60073b72cf954176f0
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-11-30 22:52:27 +0500

    back: Remove test profile configuration and consolidate application properties

--- backend/src/test/java/tech/trenero/backend/ApplicationTests.java
+++ backend/src/test/java/tech/trenero/backend/ApplicationTests.java
@@ -3,10 +3,8 @@ package tech.trenero.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ApplicationTests {
   @Test
   void shouldPassModuleVerification() {
