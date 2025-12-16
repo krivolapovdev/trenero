15f1e247b1c15368f6ec7b1cc6cb4bcb9af0e1f1
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-17 00:50:16 +0500

    mobile: Memoize useAppTheme for improved performance

--- frontend/mobile/hooks/useAppTheme.ts
+++ frontend/mobile/hooks/useAppTheme.ts
@@ -1,22 +1,27 @@
+import { useMemo } from 'react';
 import { useColorScheme } from 'react-native';
 import { MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
 
-export function useAppTheme() {
+export const useAppTheme = () => {
   const colorScheme = useColorScheme();
 
-  return colorScheme === 'light'
-    ? {
+  return useMemo(() => {
+    if (colorScheme === 'light') {
+      return {
         ...MD3LightTheme,
         colors: {
           ...MD3LightTheme.colors,
           green: '#DCEDC8'
         }
-      }
-    : {
+      };
+    } else {
+      return {
         ...MD3DarkTheme,
         colors: {
           ...MD3DarkTheme.colors,
           green: '#33691E'
         }
       };
-}
+    }
+  }, [colorScheme]);
+};
