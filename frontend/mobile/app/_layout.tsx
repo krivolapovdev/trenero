1f200327f9f214e2f52e7a75e7a7ba0e4ce9b1ca
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-17 15:54:13 +0500

    mobile: Add error boundary

--- frontend/mobile/app/_layout.tsx
+++ frontend/mobile/app/_layout.tsx
@@ -1,24 +1,28 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import ErrorBoundary from 'react-native-error-boundary';
 import { PaperProvider } from 'react-native-paper';
+import { logError } from '@/helpers/logError';
 import { useAppTheme } from '@/hooks/useAppTheme';
 
 export default function RootLayout() {
   const theme = useAppTheme();
 
   return (
-    <PaperProvider theme={theme}>
-      <StatusBar style='auto' />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name='(auth)'
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='(tabs)'
-          options={{ headerShown: false }}
-        />
-      </Stack>
-    </PaperProvider>
+    <ErrorBoundary onError={logError}>
+      <PaperProvider theme={theme}>
+        <StatusBar style='auto' />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name='(auth)'
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name='(tabs)'
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </PaperProvider>
+    </ErrorBoundary>
   );
 }
