8e3f742e6db65506a8bca9bf7f2248cf38f1a123
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-20 21:45:01 +0500

    mobile: Update student service to include `getStudentById` and create index file for exports

--- frontend/mobile/services/student/studentService.ts
+++ frontend/mobile/services/student/studentService.ts
@@ -4,8 +4,13 @@ import type { StudentResponse } from '@/services/student/student.types';
 const baseURL = '/api/v1/students';
 
 export const studentService = {
-  async getAllUsers() {
+  async getAllStudents() {
     const { data } = await api.get<StudentResponse[]>(`${baseURL}`);
+    return data;
+  },
+
+  async getStudentById(id: string) {
+    const { data } = await api.get<StudentResponse>(`${baseURL}/${id}`);
     return data;
   }
 };
