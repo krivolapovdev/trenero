b5fb74debadf516b2e22b52c344375bdd1491333
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-23 04:28:48 +0500

    back: Add @JsonUnwrapped to AuthResponse fields

--- backend/src/main/java/tech/trenero/backend/auth/internal/response/AuthResponse.java
+++ backend/src/main/java/tech/trenero/backend/auth/internal/response/AuthResponse.java
@@ -1,5 +1,7 @@
 package tech.trenero.backend.auth.internal.response;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import tech.trenero.backend.common.response.UserResponse;
 
-public record AuthResponse(UserResponse user, JwtTokenResponse jwtToken) {}
+public record AuthResponse(
+    @JsonUnwrapped UserResponse user, @JsonUnwrapped JwtTokenResponse jwtToken) {}
