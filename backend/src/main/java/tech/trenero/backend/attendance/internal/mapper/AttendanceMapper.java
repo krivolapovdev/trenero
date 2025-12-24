9e60644d860efd14b8cc2730939f1aba93f16b88
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-24 10:00:15 +0500

    back: Remove unused `toStudentAttendance` method from `AttendanceMapper` to clean up legacy code

--- backend/src/main/java/tech/trenero/backend/attendance/internal/mapper/AttendanceMapper.java
+++ backend/src/main/java/tech/trenero/backend/attendance/internal/mapper/AttendanceMapper.java
@@ -2,19 +2,6 @@ package tech.trenero.backend.attendance.internal.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
-import tech.trenero.backend.attendance.internal.entity.Attendance;
-import tech.trenero.backend.attendance.internal.request.StudentAttendanceRequest;
 
 @Mapper(componentModel = ComponentModel.SPRING)
-public interface AttendanceMapper {
-  default Attendance toStudentAttendance(StudentAttendanceRequest request) {
-    if (request == null) {
-      return null;
-    }
-
-    Attendance attendance = new Attendance();
-    attendance.setStudentId(request.studentId());
-    attendance.setPresent(request.present());
-    return attendance;
-  }
-}
+public interface AttendanceMapper {}
