0b1c8ba4ad8b481201c2a58f47f7cb7d87f39265
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-12-25 06:09:59 +0500

    mobile: Add authentication to Apollo Client with `authLink` using token from `authStore`

--- frontend/mobile/graphql/index.ts
+++ frontend/mobile/graphql/index.ts
@@ -1,8 +1,24 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { SetContextLink } from '@apollo/client/link/context';
+import { useAuthStore } from '@/stores/authStore';
+
+const httpLink = new HttpLink({
+  uri: 'http://192.168.1.4:8080/graphql'
+});
+
+const authLink = new SetContextLink(prevContext => {
+  const accessToken = useAuthStore.getState().accessToken;
+
+  return {
+    ...prevContext,
+    headers: {
+      ...prevContext.headers,
+      ...(accessToken && { Authorization: `Bearer ${accessToken}` })
+    }
+  };
+});
 
 export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'http://192.168.1.4:8080/graphql'
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
