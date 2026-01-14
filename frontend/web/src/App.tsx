387ddfe3bac9ef916bfd1be61de1f477e5fdbf56
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2026-01-14 15:49:43 +0500

    web: Use bun for web

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,53 +1,29 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import logo from './logo.svg';
+import reactLogo from './react.svg';
 
+export function App() {
   return (
-    <>
-      <div>
-        <a
-          href='https://vite.dev'
-          target='_blank'
-          rel='noopener'
-        >
-          <img
-            src={viteLogo}
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
-        <a
-          href='https://react.dev'
-          target='_blank'
-          rel='noopener'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
+    <div className='app'>
+      <div className='logo-container'>
+        <img
+          src={logo}
+          alt='Bun Logo'
+          className='logo bun-logo'
+        />
+        <img
+          src={reactLogo}
+          alt='React Logo'
+          className='logo react-logo'
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button
-          type='button'
-          onClick={() => setCount(count => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+
+      <h1>Bun + React</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
-    </>
+    </div>
   );
 }
 
