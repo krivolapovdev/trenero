d2fcf1ec957013c868b967e8e1cc03bf562ef41d
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2026-01-14 05:31:46 +0500

    back: Remove @SpringBootTest from ApplicationTests.java

--- backend/src/test/java/tech/trenero/backend/ApplicationTests.java
+++ backend/src/test/java/tech/trenero/backend/ApplicationTests.java
@@ -1,11 +1,8 @@
 package tech.trenero.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.modulith.core.ApplicationModules;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
   @Test
   void shouldPassModuleVerification() {
