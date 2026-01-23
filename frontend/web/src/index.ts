3b7d605214eb18382d300a790fdb516d4c1ebfc2
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2026-01-24 02:06:30 +0500

    web: Simplify index.ts

--- frontend/web/src/index.ts
+++ frontend/web/src/index.ts
@@ -3,30 +3,7 @@ import index from './index.html';
 
 const server = serve({
   routes: {
-    // Serve index.html for all unmatched routes.
-    '/*': index,
-
-    '/api/hello': {
-      async GET(_req) {
-        return Response.json({
-          message: 'Hello, world!',
-          method: 'GET'
-        });
-      },
-      async PUT(_req) {
-        return Response.json({
-          message: 'Hello, world!',
-          method: 'PUT'
-        });
-      }
-    },
-
-    '/api/hello/:name': async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`
-      });
-    }
+    '/*': index
   },
 
   development: process.env.NODE_ENV !== 'production' && {
