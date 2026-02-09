d95954f388f7d91ce63d1f8e0f269836c5d601a2
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2026-02-09 20:05:44 +0500

    backend: Add cleanup for duplicate lesson entries and enforce `date` column constraints

--- backend/src/main/resources/db/migration/V10__change_lesson_datetime_to_date.sql
+++ backend/src/main/resources/db/migration/V10__change_lesson_datetime_to_date.sql
@@ -4,6 +4,18 @@ ALTER TABLE lessons_module.lessons
 UPDATE lessons_module.lessons
 SET date = start_date_time::date;
 
+DELETE
+FROM lessons_module.lessons
+WHERE id IN (SELECT id
+             FROM (SELECT id,
+                          ROW_NUMBER() OVER (
+                              PARTITION BY owner_id, group_id, date
+                              ORDER BY created_at ASC
+                              ) as row_num
+                   FROM lessons_module.lessons
+                   WHERE deleted_at IS NULL) t
+             WHERE t.row_num > 1);
+
 ALTER TABLE lessons_module.lessons
     ALTER COLUMN date SET NOT NULL;
 
